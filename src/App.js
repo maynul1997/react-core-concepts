d03a957a5045af423db5177d0a5f8e78202d499f
maynul1997 <mamun.harf@gmail.com>
2021-03-01 00:40:19 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>I am React Person</p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
 }
 
+function Person(){
+  const personStyle={
+    border: '4px solid green',
+    margin: '30px'
+  }
+  return (
+    <div style={personStyle}>
+      <h1>Name : Mamun Boss</h1>
+      <h3>Hero of the Year</h3>
+    </div>
+  )
+     
+}
 export default App;
